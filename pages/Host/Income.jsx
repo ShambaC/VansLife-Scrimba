8b92e214d5b774be9be9f05fdc28a8e935a06e14
ShambaC <38806897+ShambaC@users.noreply.github.com>
2023-11-03 20:23:00 +0530

    Added income tab to host

--- pages/Host/Income.jsx
+++ pages/Host/Income.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 export default function Income() {
+    const recent = [
+        { price: 720, date: "1/12/22" },
+        { price: 560, date: "10/11/22" },
+        { price: 980, date: "23/11/22" }
+    ]
+
     return (
-        <h1>This is the host income page</h1>
+        <div className="host-income-body">
+            <h2>Income</h2>
+            <p>Last <u>30 days</u></p>
+            <h1>$2,260</h1>
+            <img />
+            <div className="host-income-recent">
+                <h3>Your transactions ({recent.length})</h3>
+                <p>Last <u>30 days</u></p>
+            </div>
+            {
+                recent.map((e) => 
+                    <div className="host-income-card" key={e.price}>
+                        <h2>${e.price}</h2>
+                        <p>{e.date}</p>
+                    </div>
+                )
+            }
+        </div>
     )
 }
